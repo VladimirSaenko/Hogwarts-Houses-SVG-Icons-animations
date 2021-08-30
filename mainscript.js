dd3fb0c6adfedfdfcc4ea19e5f45d120d336a2c9
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-08-30 19:26:32 +0300

    update

--- mainscript.js
+++ mainscript.js
@@ -9,45 +9,11 @@ let title1 = document.getElementById("title1");
 let title2 = document.getElementById("title2");
 let title3 = document.getElementById("title3");
 
-function otherWise() {
-    if(window.matchMedia("screen and (min-width: 300px) and (max-width: 800px)")) {
-        lion.addEventListener('click',function() {
-            body.style.background = "yellow";
-            title1.style.visibility = "visible";
-            title1.innerHTML = "gryffindor";
-            title1.style.color = "black";
-            slytherin.style.visibility = "hidden";
-            raven.style.visibility = "hidden";
-            title2.style.visibility = "hidden";
-            title3.style.visibility = "hidden";
-        })
-        raven.addEventListener('click', function() {
-            body.style.background = "blue";
-            title3.style.visibility = "visible";
-            title3.style.color = "white";
-            title3.innerHTML = "ravenclaw";
-            slytherin.style.visibility = "hidden";
-            lion.style.visibility = "hidden";
-            title1.style.visibility = "hidden";
-        })
-        slytherin.addEventListener('click', function() {
-            body.style.background = "green";
-            title2.style.visibility = "visible";
-            title2.style.color = "white";
-            title2.innerHTML = "slytherin";
-            raven.style.visibility = "hidden";
-            lion.style.visibility = "hidden";
-            title1.style.visibility = "hidden";
-            title3.style.visibility = "hidden";
-        })
-    }
-}
-
 lion.addEventListener('mousemove', function() {
-    body.style.background = "yellow";
+    body.style.background = "red";
     title1.style.visibility = "visible";
     title1.innerHTML = "gryffindor";
-    title1.style.color = "black";
+    title1.style.color = "yellow";
     slytherin.style.visibility = "hidden";
     raven.style.visibility = "hidden";
     title2.style.visibility = "hidden";
@@ -85,7 +51,7 @@ raven.addEventListener('mouseout',function() {
 slytherin.addEventListener('mousemove', function() {
     body.style.background = "green";
     title2.style.visibility = "visible";
-    title2.style.color = "white";
+    title2.style.color = "black";
     title2.innerHTML = "slytherin";
     raven.style.visibility = "hidden";
     lion.style.visibility = "hidden";
@@ -102,5 +68,3 @@ slytherin.addEventListener('mouseout', function() {
     title2.style.visibility = "hidden";
     title3.style.visibility = "hidden";
 })
-
-otherWise();
